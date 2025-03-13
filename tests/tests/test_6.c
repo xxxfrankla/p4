365196924ead8173395bd41d0ad77244c4dfa932
RITHIK JAIN <rjain55@wisc.edu>
2025-03-12 21:05:25 -0500

    Update 2 files
    - /tests/tests/6.out
    - /tests/tests/test_6.c

--- tests/tests/test_6.c
+++ tests/tests/test_6.c
@@ -16,6 +16,5 @@ int main(int argc, char* argv[]) {
     printf(1,"XV6_TEST_ERROR XV6_TEST_ERROR Error, procpgdirinfo returned -1\n");
   }
   printf(1, "XV6_TEST_OUTPUT After vmalloc huge, 1MB - program base pages:%d huge pages:%d\n", page_cnt[0] - original_page_cnt[0], page_cnt[1] - original_page_cnt[1]);
-  printf(1, "XV6_TEST_OUTPUT Sanity checking vmalloc address %p\n", a);
     exit();
 }
